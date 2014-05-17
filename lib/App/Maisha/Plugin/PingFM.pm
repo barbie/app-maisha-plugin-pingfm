d884953074907625fdab2d629a02d2b5bc9a789f
Barbie <barbie@missbarbell.co.uk>
2014-05-17 10:12:20 +0100

    upped version

--- lib/App/Maisha/Plugin/PingFM.pm
+++ lib/App/Maisha/Plugin/PingFM.pm
@@ -3,7 +3,7 @@ package App::Maisha::Plugin::PingFM;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 #----------------------------------------------------------------------------
 # Library Modules
